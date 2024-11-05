ee7641d992a708dfdb9dabb30940f2660621563d
Aditya Jadhav <135443183+Aditya-0124@users.noreply.github.com>
2024-11-05 16:13:26 +0530

    LeetCode Question Number 334

---
+++ QueNo334.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int firstMin = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+        for(int n : nums){
+            if(n <= firstMin){
+                firstMin = n;
+            }else if(n <= secondMin){
+                secondMin = n;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
